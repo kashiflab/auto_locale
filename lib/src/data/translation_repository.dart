5a2a22781f3f809fa9dd38286c340ad5a5fd3c51
Kashif Ali Bhatti <kashif.alib42@gmail.com>
2025-02-14 17:52:53 +0500

    stdout loading added

--- lib/src/data/translation_repository.dart
+++ lib/src/data/translation_repository.dart
@@ -19,22 +19,38 @@ class TranslationRepository {
   /// @throws Exception if the source file is not found.
   Future<void> translateAndStore(
       String filePath, String sourceLang, List<String> targetLangs) async {
+    stdout.write('Reading source file...\n');
+
     final file = File(filePath);
     if (!file.existsSync()) {
       throw Exception('Source file not found');
     }
     final content = await file.readAsString();
     final Map<String, dynamic> data = json.decode(content);
+    final int totalKeys = data.keys.length;
 
     for (String targetLang in targetLangs) {
+      stdout.write('\nTranslating to ${targetLang.toUpperCase()}...\n');
       Map<String, String> translatedData = {};
+      int processedKeys = 0;
+
       for (var key in data.keys) {
         final translations =
             await api.translate(data[key], sourceLang, [targetLang]);
         translatedData[key] = translations[targetLang] ?? '';
+        processedKeys++;
+
+        // Update progress
+        final progress = (processedKeys / totalKeys * 100).toStringAsFixed(1);
+        stdout.write('\rProgress: $progress% completed');
       }
+
+      stdout.write('\nSaving ${targetLang}.json...\n');
       await storage.saveTranslation(
           filePath, sourceLang, targetLang, translatedData);
+      stdout.write('✓ ${targetLang}.json created successfully!\n');
     }
+
+    stdout.write('\n✓ All translations completed!\n');
   }
 }
